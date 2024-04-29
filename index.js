25e109f4cb49a27417c0bd87b72f2ad148a1f14a
IzanCor <167827244+IzanCor@users.noreply.github.com>
2024-04-28 18:19:53 -0600

    server con node/js
    se implemento el servidor con node/js

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-console.log('Hello world');
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+    const read = fs.createReadStream('./static/index.html');
+    read.pipe(res);
+});
+
+
+server.listen(3000);
+console.log(`Server on port ${3000}`);
 
